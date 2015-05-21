6de3b8eba4b66789e2329a8c5c22a1cdff604948
Kevin Uhlir <n0bel@n0bel.net>
2015-05-21 13:00:43 -0500

    Changed example lat/long for Minneapolis

--- Clock/Config-Example.py
+++ Clock/Config-Example.py
@@ -1,7 +1,7 @@
 from GoogleMercatorProjection import LatLng
 
 wuprefix = 'http://api.wunderground.com/api/'
-wulocation = LatLng(44.7212951,-93.2008627)
+wulocation = LatLng(44.9764016,-93.2486732)
 noaastream = 'http://audioplayer.wunderground.com:80/tim273/edina'
 background = 'images/clockbackground-kevin.png'
 squares1 = 'images/squares1-kevin.png'
@@ -15,34 +15,24 @@ sechand = 'images/sechand.png'
 
 
 radar1 = {
-    'center' : LatLng(44.7212951,-93.2008627),
+    'center' : LatLng(44.9764016,-93.2486732),
     'zoom' : 7,
     'markers' : (
         {
-        'location' : LatLng(44.7212951,-93.2008627),
+        'location' : LatLng(44.9764016,-93.2486732),
         'color' : 'red',
         'size' : 'small',
         },
-        {
-        'location' : LatLng(44.280383,-92.983832),
-        'color' : 'red',
-        'size' : 'small',
-        },
-        {
-        'location' : LatLng(44.276069,-93.289238),
-        'color' : 'red',
-        'size' : 'small',
-        }
         )
     }
 
     
 radar2 = {
-    'center' : LatLng(44.7212951,-93.2008627),
+    'center' : LatLng(44.9764016,-93.2486732),
     'zoom' : 11,
     'markers' : (
         {
-        'location' : LatLng(44.7212951,-93.2008627),
+        'location' : LatLng(44.9764016,-93.2486732),
         'color' : 'red',
         'size' : 'small',
         },
@@ -51,24 +41,14 @@ radar2 = {
 
     
 radar3 = {
-    'center' : LatLng(44.7212951,-93.2008627),
+    'center' : LatLng(44.9764016,-93.2486732),
     'zoom' : 7,
     'markers' : (
         {
-        'location' : LatLng(44.7212951,-93.2008627),
+        'location' : LatLng(44.9764016,-93.2486732),
         'color' : 'red',
         'size' : 'small',
         },
-        {
-        'location' : LatLng(44.280383,-92.983832),
-        'color' : 'red',
-        'size' : 'small',
-        },
-        {
-        'location' : LatLng(44.276069,-93.289238),
-        'color' : 'red',
-        'size' : 'small',
-        }
         )
     }
 
