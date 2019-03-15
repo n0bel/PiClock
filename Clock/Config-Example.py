bcb5dd5980d0fb219c2ce55c044ed755d2efa492
Bert <BertLindeman@users.noreply.github.com>
2019-03-15 20:37:21 +0100

    update for #137 Add config for Radar animation speed

--- Clock/Config-Example.py
+++ Clock/Config-Example.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+     Configuration module for PyQt clock
+"""
+
 from GoogleMercatorProjection import LatLng
 from PyQt4.QtGui import QColor
 
@@ -20,13 +26,19 @@ hourhand = 'images/hourhand.png'
 minhand = 'images/minhand.png'
 sechand = 'images/sechand.png'
 
+# SlideShow
+useslideshow = 0             # 1 to enable, 0 to disable
+slide_time = 305              # in seconds, 3600 per hour
+slides = 'images/slideshow'   # the path to your local images
+slide_bg_color = "#000"       # https://htmlcolorcodes.com/  black #000
 
 digital = 0             # 1 = Digtal Clock, 0 = Analog Clock
 
 # Goes with light blue config (like the default one)
 digitalcolor = "#50CBEB"
-digitalformat = "{0:%I:%M\n%S %p}"  # The format of the time
+digitalformat = "{0:%I:%M\n%S %p}"  # Format of the digital clock face
 digitalsize = 200
+
 # The above example shows in this way:
 #  https://github.com/n0bel/PiClock/blob/master/Documentation/Digital%20Clock%20v1.jpg
 # ( specifications of the time string are documented here:
@@ -37,15 +49,17 @@ digitalsize = 200
 #  The above example shows in this way:
 #  https://github.com/n0bel/PiClock/blob/master/Documentation/Digital%20Clock%20v2.jpg
 
+digitalformat2 = "{0:%H:%M:%S}"  # Format of the digital time on second screen
 
+usemapbox = 0   # Use Mapbox.com for maps, needs api key (mbapi in ApiKeys.py)
 metric = 0  # 0 = English, 1 = Metric
 radar_refresh = 10      # minutes
+radarIMGinterval = 200  # interval between each frame of the animations in milliseconds
+radarSETinterval = 5    # interval between animations (n times radarIMGinterval)
+
 weather_refresh = 30    # minutes
 # Wind in degrees instead of cardinal 0 = cardinal, 1 = degrees
 wind_degrees = 0
-# Depreciated: use 'satellite' key in radar section, on a per radar basis
-# if this is used, all radar blocks will get satellite images
-satellite = 0
 
 # gives all text additional attributes using QT style notation
 # example: fontattr = 'font-weight: bold; '
@@ -73,35 +87,49 @@ DateLocale = ''
 LPressure = "Pressure "
 LHumidity = "Humidity "
 LWind = "Wind "
-Lgusting = " gusting "
+Lgusting = " gust "
 LFeelslike = "Feels like "
-LPrecip1hr = " Precip 1hr:"
+LPrecip1hr = " Precip 1hr: "
 LToday = "Today: "
-LSunRise = "Sun Rise:"
+LSunRise = "Sun Rise: "
 LSet = " Set: "
-LMoonPhase = " Moon Phase:"
+LMoonPhase = " Moon: "
 LInsideTemp = "Inside Temp "
 LRain = " Rain: "
 LSnow = " Snow: "
-
+Lmoon1 = 'New Moon'
+Lmoon2 = 'Waxing Crescent'
+Lmoon3 = 'First Quarter'
+Lmoon4 = 'Waxing Gibbous'
+Lmoon5 = 'Full Moon'
+Lmoon6 = 'Waning Gibbous'
+Lmoon7 = 'Third Quarter'
+Lmoon8 = 'Waning Crecent'
 
 # RADAR
 # By default, primary_location entered will be the
 #  center and marker of all radar images.
-# To update centers/markers, change radar sections below the desired lat/lon as:
-# -FROM-
-# primary_location,
-# -TO-
-# LatLng(44.9764016,-93.2486732),
+# To update centers/markers, change radar sections
+# below the desired lat/lon as:
+#    -FROM-
+#    primary_location,
+#    -TO-
+#    LatLng(44.9764016,-93.2486732),
 radar1 = {
     'center': primary_location,  # the center of your radar block
-    'zoom': 7,  # this is a google maps zoom factor, bigger = smaller area
-    'satellite': 0,    # 1 => show satellite images (colorized IR images)
+    'zoom': 7,  # this is a maps zoom factor, bigger = smaller area
+    'style': 'mapbox/satellite-streets-v10',  # optional style (mapbox only)
+    'color': 6,  # rainviewer radar color style:
+                 # https://www.rainviewer.com/api.html#colorSchemes
+    'smooth': 1,  # rainviewer radar smoothing
+    'snow': 1,  # rainviewer radar show snow as different color
     'markers': (   # google maps markers can be overlayed
         {
+            'visible': 1, # 0 = hide marker, 1 = show marker
             'location': primary_location,
             'color': 'red',
             'size': 'small',
+            'image': 'teardrop-dot',  # optional image from the markers folder
         },          # dangling comma is on purpose.
     )
 }
@@ -110,12 +138,17 @@ radar1 = {
 radar2 = {
     'center': primary_location,
     'zoom': 11,
-    'satellite': 0,
+    'style': 'mapbox/satellite-streets-v10',
+    'color': 6, 
+    'smooth': 1,
+    'snow': 1,
     'markers': (
         {
+            'visible': 1,
             'location': primary_location,
             'color': 'red',
             'size': 'small',
+            'image': 'teardrop-dot',
         },
     )
 }
@@ -124,12 +157,17 @@ radar2 = {
 radar3 = {
     'center': primary_location,
     'zoom': 7,
-    'satellite': 0,
+    'style': 'mapbox/satellite-streets-v10',
+    'color': 6, 
+    'smooth': 1,
+    'snow': 1,
     'markers': (
         {
+            'visible': 1,
             'location': primary_location,
             'color': 'red',
             'size': 'small',
+            'image': 'teardrop-dot',
         },
     )
 }
@@ -137,12 +175,17 @@ radar3 = {
 radar4 = {
     'center': primary_location,
     'zoom': 11,
-    'satellite': 0,
+    'style': 'mapbox/satellite-streets-v10',
+    'color': 6, 
+    'smooth': 1,
+    'snow': 1,
     'markers': (
         {
+            'visible': 1,
             'location': primary_location,
             'color': 'red',
             'size': 'small',
+            'image': 'teardrop-dot',
         },
     )
 }
