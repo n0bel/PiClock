af5a78559fb785b6e7ab2807006959744493fe59
Kevin Uhlir <n0bel@n0bel.net>
2020-12-24 14:34:31 -0600

    update apikeys

--- Clock/ApiKeys-example.py
+++ Clock/ApiKeys-example.py
@@ -6,6 +6,7 @@ googleapi = 'YOUR GOOGLE MAPS API KEY'
 # Mapbox API key (access_token) [if usemapbox is set in Config]
 mbapi = 'YOUR MAPBOX ACCESS TOKEN'
 # If you want to use Openweathermap.org, uncomment and add appid
+# also comment/remove ccapi if you're using Openweathermap
 # owmapi = 'YOUR OPENWEATHERMAP APPID'
 # If you want to use Climacell.co, uncomment and add appid
 ccapi = 'YOUR CLIMACELL API KEY'
