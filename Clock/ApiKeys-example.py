501658510cc0387a2668a85186622a19709d0ae7
Kevin Uhlir <n0bel@n0bel.net>
2023-04-02 09:40:38 -0500

    Update ApiKeys-example for current api options

--- Clock/ApiKeys-example.py
+++ Clock/ApiKeys-example.py
@@ -1,12 +1,15 @@
 # change this to your API keys
-
+#
 # Map API keys -- only needs 1 of the following
 # Google Maps API key (if usemapbox is not set in Config)
 googleapi = 'YOUR GOOGLE MAPS API KEY'
+#
 # Mapbox API key (access_token) [if usemapbox is set in Config]
 mbapi = 'YOUR MAPBOX ACCESS TOKEN'
+#
 # If you want to use Openweathermap.org, uncomment and add appid
-# also comment/remove ccapi if you're using Openweathermap
-# owmapi = 'YOUR OPENWEATHERMAP APPID'
-# If you want to use Climacell.co, uncomment and add appid
-ccapi = 'YOUR CLIMACELL API KEY'
+owmapi = 'YOUR OPENWEATHERMAP APPID'
+#
+# If you want to use tomorrow.io, uncomment and add appid
+# also comment/remove owmapi if you're using tomorrow.io
+# tmapi = 'YOUR tomorrow.io API KEY'
